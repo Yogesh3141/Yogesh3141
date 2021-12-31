5492d6ff6140301c5b65b28abb61edda205cd548
SkumarBirla <68109825+SharadBirla@users.noreply.github.com>
2021-12-31 23:30:00 +0530

    Update assig5.java

--- assig5.java
+++ assig5.java
@@ -5,21 +5,23 @@ class Simple_Decryption {
         File f1 = new File("C://Users/Skumar Birla/Desktop/test1.txt");
         if (f1.exists()) {
             BufferedReader br = new BufferedReader(new FileReader(f1));
-            String str, data = "";
+            String str, data = "",ddata="";
             while ((str=br.readLine()) != null) {
                 data += str;
             }
             br.close();
-            System.out.println(data);
             for(int i=1;i<data.length();i=i+2){
                 int ind=Integer.valueOf(data.substring(i-1, i+1));
-                System.out.print(alphabet[ind]);
+                ddata+=alphabet[ind];           
             }
-
+            FileWriter w1 = new FileWriter("C://Users/Skumar Birla/Desktop/test1.txt");
+            System.out.println(ddata);
+            w1.write(ddata);
+            w1.close();
         }
         else {
             System.out.println("please check file Directory, file not Exist");
 			System.out.println(f1.getAbsolutePath());
-        }
+        } 
     }
 }
